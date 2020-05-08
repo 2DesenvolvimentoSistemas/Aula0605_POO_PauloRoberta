e7ccb395e1d87969f3c4bfd52bb7ca0abdf293e8
ANTONIO <ANTONIO@10.0.0.103>
2020-05-08 14:52:02 -0300

    commit - inicio da codificacao TesteProduto

---
+++ src/pct/TesteProduto.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pct;
+
+/**
+ *
+ * @author ROBERTA
+ */
+public class TesteProduto {
+    
+    //criacao do metodo main
+    public static void main(String[] args) {
+         
+        //criacao e instancia o objeto da classe prouto
+        Produto p = new Produto();
+        
+        
+    }
+}
